5e37c11d58d75289d88c50e01a608740c0f48d7f
Jamie <rjamie.leask@gmail.com>
2017-09-27 12:18:59 -0400

    changed schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,8 @@
+DROP DATABASE IF EXISTS pizzas_db;
 
 CREATE DATABASE pizzas_db;
 USE pizzas_db;
-
+DROP TABLE IF EXISTS pizzaCreation;
 CREATE TABLE pizzaCreation (
 	id INTEGER(11) AUTO_INCREMENT NOT NULL,
 	pizza_name VARCHAR(50),
